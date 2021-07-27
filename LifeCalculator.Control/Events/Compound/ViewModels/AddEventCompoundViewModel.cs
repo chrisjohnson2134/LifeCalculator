991d60403b507072da259d0e735a033196ccefba
chrisjohnson2134 <cj106@evansville.edu>
2021-07-26 21:57:33 -0500

    Grids where removed from views where unecessary, example of hide button was put on drop down.

--- LifeCalculator.Control/Events/Compound/ViewModels/AddEventCompoundViewModel.cs
+++ LifeCalculator.Control/Events/Compound/ViewModels/AddEventCompoundViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using LifeCalculator.Control.Events;
 using LifeCalculator.Framework.Account;
 using LifeCalculator.Framework.BaseVM;
+using LifeCalculator.Framework.Enums;
 using LifeCalculator.Framework.LifeEvents;
 using Microsoft.VisualStudio.PlatformUI;
 using System;
+using System.Collections.Generic;
 
 namespace LifeCalculator.Control.ViewModels
 {
@@ -39,6 +41,7 @@ namespace LifeCalculator.Control.ViewModels
 
         public string EventName { get; set; }
         public DateTime EventDate { get; set; }
+        public DateTime EndDate { get; set; }
         public bool StartEvent { get; set; }
         public bool StopEvent { get; set; }
         public double AmountToContribute { get; set; }
@@ -46,6 +49,9 @@ namespace LifeCalculator.Control.ViewModels
         public DelegateCommand AddEventCommand { get; set; }
 
         public event EventHandler<IAccountEvent> EventAdded;
+        public List<string> EventTypes { get; set; }
+        public string EventSelected { get; set; }
+
 
         #endregion
 
@@ -62,11 +68,27 @@ namespace LifeCalculator.Control.ViewModels
                 InterestRate = InterestValue * .01
             };
 
+            EventTypes = new List<string> { "One-Time", "Monthly" };
+
             _account.AddLifeEvent(accountEvent);
 
             _account.Calculation();
 
             EventAdded?.Invoke(this, accountEvent);
+        }
+
+        #endregion
+
+        #region Helper Method
+
+        private LifeEnum EventSelectedToLifeEnum(string eventSelected)
+        {
+            if (eventSelected.Equals("One-Time"))
+                return LifeEnum.OneTime;
+            else if (eventSelected.Equals("Monthly"))
+                return LifeEnum.MonthlyContribute;
+
+            return LifeEnum.MonthlyContribute;
         }
 
         #endregion
