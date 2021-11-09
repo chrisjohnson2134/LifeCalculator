b8c201139214d33091f388f2a341e04791f072de
chrisjohnson2134 <cj106@evansville.edu>
2021-11-09 00:48:30 -0600

    Modifications to Event Views and ViewModels

--- LifeCalculator.Framework/Enums/CommonEnums.cs
+++ LifeCalculator.Framework/Enums/CommonEnums.cs
@@ -14,10 +14,8 @@
 
     public enum LifeEnum
     {
-        StartLifeEvent,
         OneTime,
-        MonthlyContribute,
-        EndLifeEvent
+        MonthlyContribute
     }
 
     public enum AccountTypes
